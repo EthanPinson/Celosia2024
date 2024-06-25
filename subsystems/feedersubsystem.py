6198650ea109282cd33192f8affd49d33686d616
mesler1 <mesler@gmail.com>
2024-06-24 19:32:16 -0500

    Clark plays with creating commands in subsystems.

--- subsystems/feedersubsystem.py
+++ subsystems/feedersubsystem.py
@@ -1,4 +1,5 @@
-from commands2 import Subsystem
+from commands2 import Subsystem, Command
+import commands2.cmd
 from constants import FeederConstants as FC
 from rev import CANSparkMax
 
@@ -11,3 +12,21 @@ class FeederSubsystem(Subsystem):
 
         self.__upperMotor = CANSparkMax(FC.UPPER_ID)
         self.__lowerMotor = CANSparkMax(FC.LOWER_ID)
+
+    def setSpeed(self):
+        self.__lowerMotor.set(FC.NOMINAL_SPEED)
+        self.__upperMotor.set(FC.NOMINAL_SPEED)
+
+    def stop(self):
+        self.__lowerMotor.set(0.)
+        self.__upperMotor.set(0.)
+
+    def runFeeder(self) -> Command:
+        return commands2.cmd.runOnce(
+            lambda: self.setSpeed()
+        )
+    
+    def stopFeeder(self) -> Command:
+        return commands2.cmd.runOnce(
+            lambda: self.stop()
+        )
